2b60995734870b114c6b639c8c775d6ac700cceb
Ganthology <gbk1386@gmail.com>
2021-01-08 14:15:44 +0800

    Add check letters that are already guessed into the input validation method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,13 +11,16 @@ word_length.times do
   display << '_'
 end
 
-def valid_input(input)
+def valid_input(input, guessed_letters)
   if input.length != 1
     puts 'Too many letters, enter only one!'
-    return false
+    false
   elsif !input.match?(/[a-z]/)
     puts 'Not alphabet letter, please enter an alphabet letter!'
-    return false
+    false
+  elsif guessed_letters.include?(input)
+    puts "You've already guessed the letter, please enter another letter!"
+    false
   else
     true
   end
@@ -26,35 +29,41 @@ end
 
 # start the game
 wrong_guesses = 10
-guesses = []
+guessed_letters = []
+guessed_letters_display = []
 until secret_word == display || wrong_guesses.negative?
   # display current results
   display.each do |char|
     print "#{char} "
   end
+
   puts "\nGuess a letter"
   guess = gets.chomp.downcase
 
-  until valid_input(guess)
+  until valid_input(guess, guessed_letters)
     puts "\nGuess a letter"
     guess = gets.chomp.downcase
   end
 
   if secret_word.include?(guess)
     puts Rainbow('Good guess!').green
-    guesses << Rainbow(guess).green
+    guessed_letters << guess
+    guessed_letters_display << Rainbow(guess).green
     indexes = secret_word.each_index.select { |i| secret_word[i] == guess }
     indexes.each do |index|
       display[index] = guess
     end
   else
     puts Rainbow('Wrong guess!').red
-    guesses << Rainbow(guess).red
+    guessed_letters << guess
+    guessed_letters_display << Rainbow(guess).red
     wrong_guesses -= 1
   end
-  puts "Wrong guesses left: #{wrong_guesses}"
+
+  puts "Wrong guesses left: #{wrong_guesses}" if wrong_guesses.positive?
+
   print 'Letters guessed: '
-  guesses.each do |char|
+  guessed_letters_display.each do |char|
     print "#{char} "
   end
   puts "\n\n"
